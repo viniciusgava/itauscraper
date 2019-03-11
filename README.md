7d550caa2c03bc7d3cf4eb28d3e9ccb33b344237
Vinicius Gava <gava.vinicius@gmail.com>
2019-03-11 01:38:10 -0300

    First version itau ofx scraper using node puppeteer

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# itauscraper
+# Itau Scraper
 Download Itaú OFX file using node and Puppeteer.
+
+## Usage
+```bash
+node run.js --branch=0000 --account=00000-0 --password=000000 --days 5 
+```
+
+## Help
+```text
+Usage: node run.js [options]
+
+Options:
+  --help          Show help                                            [boolean]
+  --version       Show version number                                  [boolean]
+  --branch, -b    Itaú branch number, format: 0000           [string] [required]
+  --account, -c   Itaú account number, format: 00000-0       [string] [required]
+  --password, -p  Itaú account digital password(6 digits)    [number] [required]
+  --days, -d      Transaction log days                       [number] [required]
+  --node_env      Node environment
+                  [choices: "development", "production"] [default: "production"]
+```
---
+++ run.js
@@ -0,0 +1,48 @@
+const nconf = require('nconf');
+const itauscraper = require('./itauscraper.js');
+
+var argv = require('yargs')
+    .env()
+    .usage('Usage: node $0 [options]')
+    .option('branch', {
+        'alias': 'b',
+        'describe': 'Itaú branch number, format: 0000',
+        'required': true,
+        type: 'string'
+    })
+    .option('account', {
+        'alias': 'c',
+        'describe': 'Itaú account number, format: 00000-0',
+        'required': true,
+        type: 'string'
+    })
+    .option('password', {
+        'alias': 'p',
+        'describe': 'Itaú account digital password(6 digits)',
+        'required': true,
+        type: 'number'
+    })
+    .option('days', {
+        'alias': 'd',
+        'describe': 'Transaction log days',
+        'required': true,
+        type: 'number'
+    })
+    .option('node_env', {
+        'describe': 'Node environment',
+        'default': 'production',
+        choices: ['development', 'production']
+    })
+;
+
+// Config
+nconf.env().argv(argv);
+const environment = nconf.get('node_env');
+nconf.file(environment, './config/' + environment.toLowerCase() + '.json');
+nconf.file('default', './config/default.json');
+
+const options = nconf.get();
+
+console.log('Starting using node environment: ' + environment);
+// Run
+itauscraper(options);
